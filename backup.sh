dbc6e5b3a1e69bf5e544ec94441be25a1f1bedaa
tbejos <txb2@live.com>
2018-07-12 01:14:10 +0000

    Easy backup

---
+++ backup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+########## .config ##########
+
+cp -r ~/.config/bspwm ~/git/dotfiles/home/tbejos/.config
+cp -r ~/.config/sxhkd ~/git/dotfiles/home/tbejos/.config
+cp -r ~/.config/polybar ~/git/dotfiles/home/tbejos/.config
+cp -r ~/.config/pulse/daemon.conf ~/git/dotfiles/home/tbejos/.config
+cp -r ~/.config/compton.conf ~/git/dotfiles/home/tbejos/.config
+
+########## home ##########
+
+cp -r ~/.xbindkeysrc ~/git/dotfiles/home/tbejos/
+cp -r ~/.xinitrc ~/git/dotfiles/home/tbejos/
+cp -r ~/.Xresources ~/git/dotfiles/home/tbejos/
+cp -r ~/.fehbg ~/git/dotfiles/home/tbejos/
+cp -r ~/Wallpapers/city.jpg ~/git/dotfiles/home/tbejos/
+
+########## boot ##########
+
+cp -r /boot/loader/entries ~/git/dotfiles/boot/loader/
+
+########## etc ##########
+
+cp -r /etc/X11/xorg.conf.d/10-invertscroll.conf ~/git/dotfiles/etc/X11/xorg.conf.d/
