4dee65cc5c69559402c2cd110bbd0f577c0383d9
tbejos <txb2@live.com>
2018-07-14 16:55:42 +0000

    user.js

--- restore.sh
+++ restore.sh
@@ -2,5 +2,6 @@
 
 cp -r ~/git/dotfiles/home/tbejos/. ~/
 cp -r ~/git/dotfiles/home/tbejos/.config ~/
+cp -r ~/git/dotfiles/home/tbejos/.mozilla/firefox/user.default ~/.mozzilla/firefox/*.default/
 sudo cp -r ~/git/dotfiles/etc ~/etc
 sudo cp -r ~/git/dotfiles/boot ~/boot
